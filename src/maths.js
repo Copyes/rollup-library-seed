2559fb5b6b1a101ac501f45348459c2e83d0ff57
Copyes <fan_545697167@qq.com>
2018-05-17 11:03:09 +0800

    style: test

--- src/maths.js
+++ src/maths.js
@@ -1,12 +1,11 @@
 // This function isn't used anywhere, so
 // Rollup excludes it from the bundle...
-const PI = 3.14
+const PI = 3.14;
 
 export function square(x) {
-  return x * x
+  return x * x;
 }
-b = 1
 // This function gets included
 export function cube(x) {
-  return x * x * x * PI
+  return x * x * x * PI;
 }
