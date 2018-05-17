c492ce09c9838135a2ea043c42ba28d36b0369ce
Copyes <fan_545697167@qq.com>
2018-05-17 11:43:05 +0800

    :construction_worker: chore: update the document ,add the git emoji

--- .cz-config.js
+++ .cz-config.js
@@ -3,41 +3,41 @@
 module.exports = {
   types: [
     {
-      value: 'WIP',
+      value: ':construction: WIP',
       name: '💪  WIP:      Work in progress'
     },
     {
-      value: 'feat',
+      value: ':sparkles: feat',
       name: '✨  feat:     A new feature'
     },
     {
-      value: 'fix',
+      value: ':bug: fix',
       name: '🐞  fix:      A bug fix'
     },
     {
-      value: 'refactor',
+      value: ':hammer: refactor',
       name: '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
     },
     {
-      value: 'docs',
+      value: ':memo: docs',
       name: '📚  docs:     Documentation only changes'
     },
     {
-      value: 'test',
+      value: ':white_check_mark: test',
       name: '🏁  test:     Add missing tests or correcting existing tests'
     },
     {
-      value: 'chore',
+      value: ':construction_worker: chore',
       name:
         "🗯  chore:    Changes that don't modify src or test files. Such as updating build tasks, package manager"
     },
     {
-      value: 'style',
+      value: ':art: style',
       name:
         '💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
     },
     {
-      value: 'revert',
+      value: ':rewind: revert',
       name: '⏪  revert:   Revert to a commit'
     }
   ],
---
+++ README.md
@@ -0,0 +1,39 @@
+### Introduction
+
+A real comfortable toolkit for building library. This toolkit is based on rollup, because it will generate a tiny file.
+
+### Features
+
+* [x] 1、Checking the file which has been modified
+* [x] 2、Formatting the code style.
+* [x] 3、Formatting submission information.
+* [x] 4、Generating multiple specification files.
+* [ ] 5、Running test and Testing your code.
+
+### Usage
+
+**git**
+
+```js
+git add xxxx
+```
+
+```js
+npm run cz || yarn run cz
+```
+
+**rollup**
+
+```js
+npm run dev || yarn run dev
+```
+
+```js
+npm run build || yarn run build
+```
+
+### Thanks
+
+[手牵手使用 Husky & Nodejs 自定义你的 Git 钩子](https://github.com/PaicFE/blog/issues/10)
+
+[用工具思路来规范化 git commit message](https://github.com/pigcan/blog/issues/15)
